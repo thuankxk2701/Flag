51d38e7f8a19e099abe4602165e94820e31b03f3
Thuan <ttctde@gmail.com>
2022-03-06 20:20:43 +0700

    Flag Countries Austrain

---
+++ Austrian.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+speed(0)
+
+setup(width=1000,height=600)
+bgcolor("red")
+
+penup()
+goto(-500,-80)
+pendown()
+
+color("white")
+
+begin_fill()
+forward(1000)
+left(90)
+forward(167)
+left(90)
+forward(1000)
+end_fill()
+
+hideturtle()
+
+done()
